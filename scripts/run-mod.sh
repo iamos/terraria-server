b0dcc0576d15ccf21de7ff8bc290002f1515831c
iamos <ohseok158@gmail.com>
2021-06-05 00:38:54 +0900

    fix; run-mod -> docker volumn mounting

--- scripts/run-mod.sh
+++ scripts/run-mod.sh
@@ -1,7 +1,7 @@
 docker run -it --restart=unless-stopped \
 -p 7777:7777 \
--v /home/iamos/terraria/modloader/Worlds:/terraria/world \
--v /home/iamos/terraria/modloader/Mods:/terraria/mods \
+-v /home/iamos/terraria/modloader/Worlds:/terraria/World \
+-v /home/iamos/terraria/modloader/Mods:/terraria/Mods \
 --name terraria-mod \
 iamos/terraria-tmodloader:latest
 
